de9112e74ed70445d48535b4e7e777812481d402
john-poe <31974547+john-poe@users.noreply.github.com>
2021-10-17 15:31:31 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,6 @@ const Hero = () => (
   <div className="container py-12 lg:pb-16">
     <h2 className="text-3xl sm:text-4xl font-extrabold leading-tight tracking-tight text-gray-900">
       A Hogwarts Site{" "}
-      <span role="img" aria-label="waving hand">
-        👋
-      </span>
       <br />
       <span className="text-blue-600">
         Welcome to all things Hogwarts.
